3003c12e91859e83dddbff73e2b3cc0e327c2600
FatChicken277 <1637@holbertonschool.com>
2020-10-04 18:12:13 -0500

    change values and functions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,19 +14,19 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square """
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation of the square """
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
