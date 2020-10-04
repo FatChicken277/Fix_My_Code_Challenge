9b3d5ef52319173f8cb1850f88b5099ef5220c11
FatChicken277 <1637@holbertonschool.com>
2020-10-04 18:28:51 -0500

    change some code conventions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Tjis module containing an object square """
+""" This module containing an object square """
 
 
-class square():
+class Square():
     """ Square object """
     width = 0
     height = 0
 
-    def __init__(self, width, height):
+    def __init__(self, *args, **kwargs):
         """ Initialize square"""
-        self.width = width
-        self.height = width
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -26,7 +26,7 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
